30de56b780f0c40d8027c9733f2162023364cced
guihibb <109474896+guihibb@users.noreply.github.com>
2023-12-29 14:04:17 -0300

    ipdate

---
+++ src/app/navigation/campanha/page.tsx
@@ -0,0 +1,11 @@
+import React, { useState } from 'react';
+import EditarPerfil from '../../components/editarperfil';
+import '../../css/perfil.css'
+
+
+export default function Home() {
+    return (
+      <></>
+    )
+  }
+
