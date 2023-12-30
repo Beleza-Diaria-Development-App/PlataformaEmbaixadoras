60702385f141fe24ad17ccff5708e1a2063d1133
guihibb <109474896+guihibb@users.noreply.github.com>
2023-12-30 14:21:22 -0300

    upp

--- src/app/navigation/campanha/page.tsx
+++ src/app/navigation/campanha/page.tsx
@@ -1,11 +1,22 @@
 import React, { useState } from 'react';
 import EditarPerfil from '../../components/editarperfil';
-import '../../css/perfil.css'
+import Navbar from "../../components/navbar"
+import '../../css/css_home.css'
+import "../../css/campanha.css"
 
 
 export default function Home() {
     return (
-      <></>
-    )
+      <section className=''>
+        <Navbar />
+        <div className="letreiro-welcome">
+            <h1 className='tt-welcome'>Seja Bem Vinda a Plataforma</h1>
+            <div className='bd-txt-welcome'>
+              <h1 className='tt-welcome-full'>Beleza Diária</h1>
+              <p className='tt-p'>Explore um mundo de cuidados personalizados para realçar sua beleza a cada dia.</p>
+            </div>
+          </div>
+      </section>
+  )
   }
 
